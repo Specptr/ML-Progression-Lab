6b94a909c4cfd8711d6001ffff87920390c554e6
EnoLaice <152045548+Specptr@users.noreply.github.com>
2025-12-05 23:51:13 +0000

    Add main_univariate.py for univariate regression

--- 01_linear_regression/main_univariate.py
+++ 01_linear_regression/main_univariate.py
@@ -1,13 +1,13 @@
 # 2025.12.5
 import numpy as np
 from linear_regression import LinearRegressionGD
-from visualize import plot
+from visualize import plot_univariate
 
 ALPHA = 0.01
 ITERATIONS = 1500
 
 def load_dataset():
-    data = np.loadtxt("data/data.txt", delimiter=",")
+    data = np.loadtxt("data/data1.txt", delimiter=",")
     X = data[:, 0]
     y = data[:, 1]
     return X, y
@@ -22,7 +22,7 @@ def main():
     # Predictions
     y_pred = model.predict(X)
 
-    plot(X, y, y_pred, model.cost_history, model, theta=model.theta)
+    plot_univariate(X, y, y_pred, model.cost_history, model, theta=model.theta)
     print("θ learned:", model.theta)
 
 if __name__ == "__main__":
